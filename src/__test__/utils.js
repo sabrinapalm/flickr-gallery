b1e980cbf37519d670839083190051a3b0559882
sabrinapalm <sabrina_palm@hotmail.com>
2019-10-21 01:41:33 +0200

    added go to top button, new css and more tests

--- src/__test__/utils.js
+++ src/__test__/utils.js
@@ -1,3 +1,17 @@
 exports.generateApiUrl = (settings, page_num) => {
-	return `${settings.baseUrl}api_key=${settings.api_key}&format=${settings.format}&method=${settings.method}&tags=${settings.tags}&media=${settings.media}&privacy_filter=${settings.privacy_filter}&nojsoncallback=${settings.nojsoncallback}&per_page=${settings.per_page}&extras=${settings.extras}&page=${page_num}&safe_search=${settings.safe_search}`;
+    if (settings === undefined || settings === null) {
+        throw new Error('Not an object!');
+    }
+
+    if (isNaN(page_num)) {
+        throw new Error('Not a number');
+    }
+    return `${settings.baseUrl}api_key=${settings.api_key}&format=${settings.format}&method=${settings.method}&tags=${settings.tags}&media=${settings.media}&privacy_filter=${settings.privacy_filter}&nojsoncallback=${settings.nojsoncallback}&per_page=${settings.per_page}&extras=${settings.extras}&page=${page_num}&safe_search=${settings.safe_search}`;
+};
+
+exports.searchNewTag = value => {
+    if (typeof value !== 'string') {
+        throw new Error('Not a string!');
+    }
+    return value;
 };
